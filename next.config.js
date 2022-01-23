b1667fd0db5a88df0a0ab9423eb66907ede193ee
CS-5 <me@carsonseese.com>
2022-01-23 11:09:25 -0500

    Update NPM scripts

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.API_URL ?? ""}/api/:path*`,
+        destination: `/api/:path*`,
       },
     ];
   },
