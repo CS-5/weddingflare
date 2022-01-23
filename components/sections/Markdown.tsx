3824fc9d7efad38c335598ee5ec6de2600ab19f0
CS-5 <me@carsonseese.com>
2022-01-23 11:10:27 -0500

    Markdown component

---
+++ components/sections/Markdown.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+const MarkdownSection: FunctionComponent = () => {
+  return <div></div>; //<ReactMarkdown remarkPlugins={[remarkGfm]}></ReactMarkdown>;
+};
+
+export default MarkdownSection;
